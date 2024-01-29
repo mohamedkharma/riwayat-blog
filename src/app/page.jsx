ecc0469a8401ac8de9f57207bea5d63ef72524f2
mohamedkharma <mohamedkharma@hotmail.com>
2024-01-29 00:03:21 -0500

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,20 @@
+import styles from "./homepage.module.css";
+import "./globals.css"
+import Link from "next/link";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
+
+export default function Home() {
+  return (
+  <div className={styles.container}>
+    <Featured />
+    <CategoryList />
+    <div className={styles.container}>
+      <CardList />
+      <Menu />
+    </div>
+  </div>
+  );
+}
